4fb9947b433de264b9b22d898567de2793563896
Ryan Utke <utkeryan@grinnell.edu>
2015-05-29 14:19:57 -0500

    Reset

--- bucketMultiselect.cu
+++ bucketMultiselect.cu
@@ -38,7 +38,6 @@ namespace BucketMultiselect{
       printf("Error at %s:%d\n",__FILE__,__LINE__);     \
       return EXIT_FAILURE;}} while(0)
 
- 	// Testing simultaneous
 
   /// ***********************************************************
   /// ***********************************************************
